f03e4cd90e8de709d2625252ffe0fee45dc455ad
Alvaro Leandro Garcia Vera <alvarolvgarcia@hotmail.com>
2024-05-08 00:59:20 -0300

    se agrego a la biblioteca varios elementos
    un json con contenido fotos con titulo descripcion genero y un estilo css ademas se hizo una funcion para cargar el orgen.

--- js/main.js
+++ js/main.js
@@ -4,4 +4,44 @@ window.onload = function () {
     document.getElementById('menu-toggle').addEventListener('click', function () {
         document.getElementById('menu').classList.toggle('active');
     });
+
+    const url = 'json/biblioteca.json';
+
+    const bibliotecaContainer = document.getElementById('biblioteca');
+
+    fetch(url)
+        .then(response => response.json())
+        .then(data => {
+            for (let clave in data) {
+                if (data.hasOwnProperty(clave)) {
+                    const foto = data[clave];
+
+                    const contenedor = document.createElement('div');
+                    contenedor.classList.add('foto-contenedor');
+                    contenedor.classList.add('item');
+                    
+                    const imagen = document.createElement('img');
+                    imagen.src = foto.src;
+                    imagen.alt = foto.title;
+                    imagen.classList.add('imagen');
+
+                    const texto = document.createElement('div');
+                    texto.classList.add('texto-polarizado');
+                    texto.innerHTML = `
+                        <h3>${foto.title}</h3>
+                        <p>${foto.description}</p>
+                        <p>Género: ${foto.gen}</p>
+                    `;
+
+                    contenedor.appendChild(imagen);
+                    contenedor.appendChild(texto);
+
+                    bibliotecaContainer.appendChild(contenedor);
+                }
+            }
+        })
+        .catch(error => {
+            console.error('Error al cargar el archivo JSON:', error);
+        });
+
 };
