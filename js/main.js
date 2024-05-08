d4b7232e044f87dbbddea8deb1358160255c9415
Alvaro Leandro Garcia Vera <alvarolvgarcia@hotmail.com>
2024-05-07 21:00:56 -0300

    Intento casi exitoso menu hamburguesa, y algunos media query

--- js/main.js
+++ js/main.js
@@ -1,4 +1,7 @@
-window.onload = function() {
-    
+window.onload = function () {
+
     console.log("La página se ha cargado completamente.");
+    document.getElementById('menu-toggle').addEventListener('click', function () {
+        document.getElementById('menu').classList.toggle('active');
+    });
 };
