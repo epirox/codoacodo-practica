63b40d238b94bbb834af7feee072e440a584d1f9
Alvaro Leandro Garcia Vera <alvarolvgarcia@hotmail.com>
2024-05-11 18:58:01 -0300

    sin bootstrap menu hamburgesa

--- js/main.js
+++ js/main.js
@@ -1,10 +1,24 @@
-window.onload = function () {
+let currentIndex = 0;
 
-    console.log("La página se ha cargado completamente.");
-    document.getElementById('menu-toggle').addEventListener('click', function () {
-        document.getElementById('menu').classList.toggle('active');
-    });
+const moveCarousel = function (direction) {
+    console.log(currentIndex)
+    const carouselInner = document.querySelector('.carousel-inner');
+    const carouselItems = document.querySelectorAll('.carousel-item');
 
+    const carouselWidth = carouselItems[0].offsetWidth;
+    const maxIndex = carouselItems.length - 1;
+
+    if (direction === -1 && currentIndex > 0) {
+        currentIndex--;
+    } else if (direction === 1 && currentIndex < maxIndex) {
+        currentIndex++;
+    }
+
+    const newPosition = -currentIndex * carouselWidth;
+    carouselInner.style.transform = `translateX(${newPosition}px)`;
+}
+
+const loadBibliotecaImgenes = function () {
     const url = 'json/biblioteca.json';
 
     const bibliotecaContainer = document.getElementById('biblioteca');
@@ -19,7 +33,7 @@ window.onload = function () {
                     const contenedor = document.createElement('div');
                     contenedor.classList.add('foto-contenedor');
                     contenedor.classList.add('item');
-                    
+
                     const imagen = document.createElement('img');
                     imagen.src = foto.src;
                     imagen.alt = foto.title;
@@ -43,31 +57,60 @@ window.onload = function () {
         .catch(error => {
             console.error('Error al cargar el archivo JSON:', error);
         });
+}
 
-        
+const isInViewport = function (element) {
+    const rect = element.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.left >= 0 &&
+        rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
-        const isInViewport = function(element) {           
-            const rect = element.getBoundingClientRect();
-            return (
-                rect.top >= 0 &&
-                rect.left >= 0 &&
-                rect.bottom <= (window.innerHeight || document.documentElement.clientHeight) &&
-                rect.right <= (window.innerWidth || document.documentElement.clientWidth)
-            );
+const showItems = function () {
+    const items = document.querySelectorAll('.item');
+    items.forEach(item => {
+        if (isInViewport(item)) {
+            item.classList.add('mostrar');
+        } else {
+            item.classList.remove('mostrar');
         }
+    });
+}
 
-        const showItems = function () {
-            const items = document.querySelectorAll('.item');
-            items.forEach(item => {
-                if (isInViewport(item)) {
-                item.classList.add('mostrar');
-                } else {
-                item.classList.remove('mostrar');
-                }
-            });
-        }
+const loadEventScroll = function () {
+    window.addEventListener('scroll', showItems);
+}
 
-        window.addEventListener('scroll', showItems);
+const debugLoad = function () {
+    console.log("La página se ha cargado completamente.")
+}
+
+const loadEventMenuToggle = function () {
+    document.getElementById('menu-toggle').addEventListener('click', function () {
+        document.getElementById('menu').classList.toggle('active');
+    });
+}
+
+const loadEventCarousel = function(){
+    document.getElementById('b-prev')
+    .addEventListener('click', function () {
+        moveCarousel(-1)
+    });
+    document.getElementById('b-next')
+    .addEventListener('click', function () {
+        moveCarousel(1)
+    });
+}
 
 
-};
+window.onload = function () {
+    
+    loadEventMenuToggle()
+    loadBibliotecaImgenes()
+    loadEventScroll()
+    loadEventCarousel()
+    debugLoad()
+}
